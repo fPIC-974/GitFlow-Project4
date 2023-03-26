1100d330f315b89738a7c9c806ec65495cf29372
Frederic Picard <fred.picard@gmail.com>
2023-03-26 18:08:21 +0200

    Updated with recurring users checks

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -86,4 +86,28 @@ public class TicketDAO {
         }
         return false;
     }
+
+    // FPIC-974 -
+    public int getNbTicket(String vehicleRegNumber) {
+        Connection con = null;
+        int nbTickets = 0;
+
+        try {
+            con = dataBaseConfig.getConnection();
+            PreparedStatement ps = con.prepareStatement(DBConstants.GET_NB_TICKET);
+
+            ps.setString(1,vehicleRegNumber);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                nbTickets = rs.getInt(1);
+            }
+            dataBaseConfig.closeResultSet(rs);
+            dataBaseConfig.closePreparedStatement(ps);
+        }catch (Exception ex){
+            logger.error("Error fetching number of tickets",ex);
+        }finally {
+            dataBaseConfig.closeConnection(con);
+        }
+        return nbTickets;
+    }
 }
